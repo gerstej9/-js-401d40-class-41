829c7e3bfdc3dd6eff57c33804f414ad93869468
James Gerstenberger <gerstej9@gmail.com>
2021-04-12 17:36:28 -0700

    Completed lab day 1 of react native app

---
+++ App.js
@@ -0,0 +1,81 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect }from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+import * as Location from 'expo-location';
+import bigBrother from './assets/BigBrother.jpg'
+
+export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [detailedLocation, setDetailedLocation] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let lat = '';
+  let lon = '';
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = location
+    lat = text.coords.latitude;
+    lon = text.coords.longitude;
+  }
+
+  const getDetailedLocation = async (latitude, longitude) => {
+  if(lat !== ''){
+    let newLocation = await axios.get(`https:us1.locationiq.com/v1/search.php?key=pk.ec7dd268e7db863b8ee3de2dc5489245&q=${latitude},${longitude}&format=json`);
+    setDetailedLocation(newLocation.data[0].display_name);
+  }
+}
+
+  useEffect(() => {
+    getDetailedLocation(lat, lon);
+  }, [lat]);
+
+
+  return (
+    <View style={styles.container}>
+      <Image source={bigBrother} style={styles.media}/>
+      <Text style={styles.text}>Big Brother is Watching...</Text>
+      <Text style={styles.text}>{detailedLocation}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    borderWidth: 5,
+    borderColor: "red",
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+    fontSize: 28,
+  },
+  media:{
+    width: 300,
+    height: 300,
+    flex: 2,
+  }
+});
